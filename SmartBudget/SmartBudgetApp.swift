55b345e623b3229ef23acd9d5deb4e95f59e135c
Yofrank <yofr4nk@gmail.com>
2023-08-30 23:46:17 -0400

    Initial Commit

---
+++ SmartBudget/SmartBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartBudgetApp.swift
+//  SmartBudget
+//
+//  Created by yofrank sanchez on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SmartBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
